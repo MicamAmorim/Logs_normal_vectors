79684b6a3833ab1192927d9167f4a6b9422f6a6f
MIQUÉIAS AMORIM SANTOS SILVA AMORIM <kekoamorimsilva@gmail.com>
2023-01-25 11:16:41 -0300

    final commit

--- Euler_Angles.py
+++ Euler_Angles.py
@@ -96,10 +96,7 @@ def Euler_Angles(n):
 	return eulers
 
 def set_axes_radius(ax, origin, radius):
-    '''
-        From StackOverflow question:
-        https://stackoverflow.com/questions/13685386/
-    '''
+
     ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
     ax.set_ylim3d([origin[1] - radius, origin[1] + radius])
     ax.set_zlim3d([origin[2] - radius, origin[2] + radius])
